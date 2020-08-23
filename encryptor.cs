1592fca6a108d74c00a25c490b0e65c52d044d37
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-23 11:33:03 +0300

    Changed result to be string.Empty

--- encryptor.cs
+++ encryptor.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-            string result = "";
+            string result = string.Empty;
             foreach (var arg in args)
             {
                 string resultInInt = Secret(arg);
